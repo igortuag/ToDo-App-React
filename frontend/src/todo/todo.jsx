530803e91c4a70ab29d491c6ab5f6a597083daa7
Igor Tuag <igortuag@gmail.com>
2020-05-17 16:59:03 -0300

    Component Todo and About

---
+++ frontend/src/todo/todo.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class todo extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Todo</h1>
+            </div>
+        )
+    }
+}
