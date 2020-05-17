df6ceb2ac8e1097163d9e8b7ca7568a71bb580a5
Igor Tuag <igortuag@gmail.com>
2020-05-17 17:18:04 -0300

    Menu component

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -2,11 +2,13 @@ import 'modules/bootstrap/dist/css/bootstrap.min.css'
 import 'modules/font-awesome/css/font-awesome.min.css'
 
 import React from 'react'
+import Menu from '../template/menu'
 import Todo from '../todo/todo'
 import About from '../about/about'
 
 export default props => (
     <div className='container'>
+        <Menu />
         <Todo />
         <About />
     </div>
