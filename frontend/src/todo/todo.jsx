e91ff79a9f354a007c03c74e96ff49fb247b1648
Igor Tuag <igortuag@gmail.com>
2020-05-18 08:25:10 -0300

    Config to Marked with done and pending

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -16,6 +16,8 @@ export default class todo extends Component {
         this.handleAdd = this.handleAdd.bind(this)
 
         this.handleRemove = this.handleRemove.bind(this)
+        this.handleMarkAsDone = this.handleMarkAsDone.bind(this)
+        this.handleMarkAsPending = this.handleMarkAsPending.bind(this)
 
         this.refresh()
     }
@@ -40,15 +42,31 @@ export default class todo extends Component {
             .then(resp => this.refresh())
     }
 
+    handleMarkAsDone(todo) {
+        axios.put(`${URL}/${todo._id}`, { ...todo, done: true })
+            .then(resp => this.refresh())
+    }
+
+    handleMarkAsPending(todo) {
+        axios.put(`${URL}/${todo._id}`, { ...todo, done: false })
+            .then(resp => this.refresh())
+    }
+
     render() {
         return (
             <div>
                 <PageHeader name='Tasks' small='register' />
-                <TodoForm description={this.state.description} 
+                <TodoForm 
+                    description={this.state.description} 
                     handleChange={this.handleChange}
-                    handleAdd={this.handleAdd}/>
-                <TodoList list={this.state.list}
-                    handleRemove={this.handleRemove}/>
+                    handleAdd={this.handleAdd}
+                />
+                <TodoList 
+                    list={this.state.list}
+                    handleRemove={this.handleRemove}
+                    handleMarkAsDone={this.handleMarkAsDone}
+                    handleMarkAsPending={this.handleMarkAsPending}
+                />
             </div>
         )
     }
