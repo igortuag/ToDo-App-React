d31ceeeefad496c29b51f495f0eeac724cddb63e
Igor Tuag <igortuag@gmail.com>
2020-05-19 07:01:07 -0300

    Add keyboard shortcuts

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -1,22 +1,33 @@
 import React from 'react'
-import Grid from  '../template/grid'
+import Grid from '../template/grid'
 import IconButton from '../template/iconButton'
 
-export default props => (
-    <div role='Form' className='todoForm'>
+export default props => {
+    const keyHandler = (e) => {
+        if (e.key === 'Enter') {
+            e.shiftKey ? props.handleSearch () : props.handleAdd()
+        }else if (e.key === 'Escape') {
+            props.handleClear()
+        }
+    }
 
-        <Grid cols='12 9 10'>
-            <input id="description" className='form-control' 
-                placeholder='Add some task'
-                onChange={props.handleChange} 
-                value={props.description}
-            />
-        </Grid>
+    return (
+        <div role='Form' className='todoForm'>
 
-        <Grid cols='12 3 2'>
-            <IconButton style='primary' icon='plus' onClick={props.handleAdd}/>
-            <IconButton style='info' icon='search' onClick={props.handleSearch}/>
-            <IconButton style='default' icon='close' onClick={props.handleClear}/>
-        </Grid>
-    </div>
-)
+            <Grid cols='12 9 10'>
+                <input id="description" className='form-control'
+                    placeholder='Add some task'
+                    onChange={props.handleChange}
+                    onKeyUp={keyHandler}
+                    value={props.description}
+                />
+            </Grid>
+
+            <Grid cols='12 3 2'>
+                <IconButton style='primary' icon='plus' onClick={props.handleAdd} />
+                <IconButton style='info' icon='search' onClick={props.handleSearch} />
+                <IconButton style='default' icon='close' onClick={props.handleClear} />
+            </Grid>
+        </div>
+    )
+}
