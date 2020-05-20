1203d7704574852cdf41ecb03015014c42f7b0dc
Igor Tuag <igortuag@gmail.com>
2020-05-20 19:16:38 -0300

    Config firts reducers

---
+++ frontend/src/main/reducers.js
@@ -0,0 +1,22 @@
+import { combineReducers } from 'redux'
+
+const rootReducer = combineReducers({
+    todo: () => ({
+        description: 'Ler livro',
+        list: [{
+            _id: 1,
+            description: 'Pagar fatura do cartão',
+            done: true
+        }, {
+            _id: 2,
+            description: 'Reunião com a equipe às 10:00',
+            done: false
+        }, {
+            _id: 3,
+            description: 'Consulta médica na terça depois do almoço',
+            done: false
+        }]
+    })
+})
+
+export default rootReducer
