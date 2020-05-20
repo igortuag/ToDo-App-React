c61936e5ba2f0d18fab962b1310c4edbf6ea0ae8
Igor Tuag <igortuag@gmail.com>
2020-05-20 19:20:54 -0300

    Config. Redux

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,5 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './main/app'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 
-ReactDOM.render(<App />, document.getElementById('app'))
+import App from './main/app'
+import reducer from './main/reducers'
+
+const store = createStore(reducer)
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+    , document.getElementById('app'))
