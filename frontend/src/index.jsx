fa5fc29e9c3378323bc84887de22e85af9f23796
Igor Tuag <igortuag@gmail.com>
2020-05-20 20:53:13 -0300

    Apply Middleware Redux-Promise

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -2,14 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
+import promise from 'redux-promise'
 
 import App from './main/app'
 import reducer from './main/reducers'
+import { applyMiddleware } from 'redux'
 
 const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ 
     && window.__REDUX_DEVTOOLS_EXTENSION__()
 
-const store = createStore(reducer, devTools)
+const store = applyMiddleware(promise)(createStore)(reducer, devTools)
 ReactDOM.render(
     <Provider store={store}>
         <App />
--- frontend/src/todo/todoReducer.js
+++ frontend/src/todo/todoReducer.js
@@ -1,19 +1,4 @@
-const INITIAL_STATE = {
-    description: 'Ler livro',
-    list: [{
-        _id: 1,
-        description: 'Pagar fatura do cartão',
-        done: true
-    }, {
-        _id: 2,
-        description: 'Reunião com a equipe às 10:00',
-        done: false
-    }, {
-        _id: 3,
-        description: 'Consulta médica na terça depois do almoço',
-        done: false
-    }]
-}
+const INITIAL_STATE = { description: '', list: [] }
 
 export default (state = INITIAL_STATE, action) => {
     switch(action.type) {
