4af3e1b55a9327b96f4047708681b268785f0923
Igor Tuag <igortuag@gmail.com>
2020-05-21 06:59:25 -0300

    Delete task on To Do List

--- frontend/src/todo/todoActions.js
+++ frontend/src/todo/todoActions.js
@@ -36,3 +36,10 @@ export const markAsPending = (todo) => {
             .then(resp => dispatch(search()))
     }
 }
+
+export const remove = ( todo ) => {
+    return dispatch => {
+        axios.delete(`${URL}/${todo._id}`)
+            .then(resp => dispatch(search()))
+    }
+}
